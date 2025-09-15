ae4550df916d2593dc524667d6ade00c1c788cfb
Gabriel <gabrielkuran42@gmail.com>
2025-09-14 22:35:17 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,17 +1,21 @@
 // Serene UI - Design System Components
-export { default as Accordion } from './stories/Accordion/Accordion';
-export { default as Alert } from './stories/Alert/Alert';
-export { default as Avatar } from './stories/Avatar/Avatar';
-export { default as Box } from './stories/Box/Box';
-export { default as Button } from './stories/Button/Button';
-export { default as Card } from './stories/Card/Card';
-export { default as Checkbox } from './stories/Checkbox/Checkbox';
-export { default as CloseButton } from './stories/CloseButton/CloseButton';
-export { default as FormControl } from './stories/FormControl/FormControl';
-export { default as Grid } from './stories/Grid/Grid';
-export { default as RadioButton } from './stories/RadioButton/RadioButton';
-export { default as Switch } from './stories/Switch/Switch';
-export { default as Tag } from './stories/Tag/Tag';
+export { Accordion } from './stories/Accordion/Accordion';
+export { Alert } from './stories/Alert/Alert';
+export { Avatar } from './stories/Avatar/Avatar';
+export { Box } from './stories/Box/Box';
+export { Button } from './stories/Button/Button';
+export { Card } from './stories/Card/Card';
+export { Checkbox } from './stories/Checkbox/Checkbox';
+export { CloseButton } from './stories/CloseButton/CloseButton';
+export { FormControl } from './stories/FormControl/FormControl';
+export { Grid } from './stories/Grid/Grid';
+export { RadioButton } from './stories/RadioButton/RadioButton';
+export { Switch } from './stories/Switch/Switch';
+export { Tag } from './stories/Tag/Tag';
 
 // Theme
-export { default as theme } from './theme';
+export { theme } from './theme';
+
+// Export types
+export type { ButtonProps } from './stories/Button/Button';
+export type { CardProps } from './stories/Card/Card';
